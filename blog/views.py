88f1dd8547427d62dac42c847752772b875c3f66
Moises <moises.gnzalz@gmail.com>
2016-11-17 22:27:45 -0600

    Cambie el HTML para la página.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request): #creamos una funcion post_list
+        return render(request, 'blog/post_list.html', {}) #creamos nuestra vista
