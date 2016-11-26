76733931d4191a0815b49a07da32ac72a6cabd8d
Moises <moises.gnzalz@gmail.com>
2016-11-25 20:02:29 -0600

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.post_list),
+        url(r'^$', views.post_list), #ruta hacia la vista post_list
+        url (r'^post/(?P<pk>[0-9]+)/$', views.post_detail), #lo que se llame post/ y tenga un numero y / redirige a la vista post_detail
         ]
